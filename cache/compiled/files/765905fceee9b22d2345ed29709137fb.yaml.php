249c15fb12b53af1f929b9126225a35e609afe42
Adam Greenough <adam@greenough.me>
2020-02-22 03:51:44 +0000

    💨

--- cache/compiled/files/765905fceee9b22d2345ed29709137fb.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/adam/htdocs/webwide-blog/user/config/security.yaml',
-    'modified' => 1582334586,
-    'data' => [
-        'salt' => 'IHXza04fWk1SK3'
-    ]
-];
